ca621d9f982323e7df39599968594aeb7c024f7c
Unknown <roxroy@devicode.com>
2017-08-02 00:17:17 -0400

    Update login validation

--- server/auth/passport.js
+++ server/auth/passport.js
@@ -13,24 +13,19 @@ module.exports = (passport) => {
     });
   });
 
-  passport.use('local-signup', new LocalStrategy( 
-  {
-     passReqToCallback : true // allows us to pass back the entire request to the callback
-  },
+  passport.use('local-signup', new LocalStrategy(
   (username, password, done) => {
     process.nextTick(() => {
       User.findOne({ username: username }, (err, user) => {
-      console.log('user', user);
 
         if (err) {
           return done(err);
         }
         if (user) { 
-           console.log('dup user', user);
-          return done(null, false,  { success: false, message: 'That username is already taken.'}); 
+          return done(null, false,  { message : 'That username is already taken.'}); 
         } else {
   
-         userServices.addNew({username, password}, (newUser) => {
+          userServices.addNew({username, password}, (newUser) => {
             return done(null, newUser);
           });  
         }
@@ -40,23 +35,20 @@ module.exports = (passport) => {
   
 
   passport.use('local-login', new LocalStrategy( 
-  {
-     passReqToCallback : true // allows us to pass back the entire request to the callback
-  },
-  (req, username, password, done) => {
+  (username, password, done) => {
     process.nextTick(() => {
       User.findOne({ username: username }, (err, user) => {
         if (err) {
           return done(err);
         }
         if (!user) { 
-          return done(null, false,  { success: false, message: 'Incorrect userName or Password.'}); 
+          return done(null, false,  { message: 'Incorrect userName or Password.'}); 
         }
         if (!user.validPassword(password)){
-          return done(null, false,  { success: false, message: 'Incorrect userName or Password.'}); 
+          return done(null, false,  { message: 'Incorrect userName or Password.'}); 
         }
   
-        return done(null, newUser);
+        return done(null, user);
         
       });
     });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,21 +2,33 @@ module.exports = (app, passport) => {
   app.route('/logout')
     .get((req, res) => {
       req.logout();
-      //res.redirect('/');
+      res.redirect('/');
+    }); 
+
+    app.post('/login', function(req, res, next) {
+      passport.authenticate('local-login', function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (user) {
+            return res.status(200).json({success: true});
+        } else {
+            return res.status(200).json( Object.assign({success: false, info}));
+        }
+      })(req, res, next);
     });
 
-  app.route('/login')
-    .post(passport.authenticate('local-login'),
-    (req, res) => {
-      //res.redirect('/');
-    });
+    app.post('/signup', function(req, res, next) {
+      passport.authenticate('local-signup', function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (user) {
+            return res.status(200).json({success: true});
+        } else {
+            return res.status(200).json( Object.assign({success: false, info}));
+        }
+    })(req, res, next);
+  });
 
-  app.route('/signup')
-    .post(passport.authenticate('local-signup'),
-    (err, user, info) => {
-      console.log('/signup', err);
-      if (err) return next(err);
-      if (!user) return next(null, false);
-      next(null, user);
-    });
 };
