cd1b90f19393327a09af1f5a63093a443fd5504c
Unknown <roxroy@devicode.com>
2017-08-20 16:01:24 -0400

    Upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,39 +10,39 @@ module.exports = {
     filename: 'app.js'
   },
   resolve: {
-     extensions: ['','.jsx', '.js', '.css'],
+     extensions: ['.jsx', '.js', '.css'],
 	},
   module: {
     loaders: [
       {
         test: /\.js$/, 
-        loader: 'babel-loader', 
+        use: 'babel-loader', 
         exclude: /node_modules/ 
       },
       { 
         test: /\.jsx$/, 
-        loader: 'babel-loader', 
+        use: 'babel-loader', 
         exclude: /node_modules/ 
       },
       { // regular css files
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          loader: 'css-loader?importLoaders=1',
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1',
         }),
       },
       { // sass / scss loader for webpack
         test: /\.(sass|scss)$/,
-        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
       },
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-      {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: "file"},
+      {test: /\.(woff|woff2)$/, use: "url?prefix=font/&limit=5000"},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: "url?limit=10000&mimetype=application/octet-stream"},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: "url?limit=10000&mimetype=image/svg+xml"}
     ]
   },
   // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
   plugins: [
-     new ExtractTextPlugin("app.css"),
+    new ExtractTextPlugin("app.css"),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, './src/index.html'),
     })
